878e0110c1f73885e11a4cc69264be7a50c63380
jorginazario <jnazario@nd.edu>
2017-03-08 18:31:55 -0500

    jquery push for side nav

--- initialization.js
+++ initialization.js
@@ -1,14 +1,13 @@
-$(document).ready(function(){
+$(document).ready(function() {
   $('.collapsible').collapsible();
   // Initialize collapse button
- $(".button-collapse").sideNav();
+  $(".button-collapse").sideNav();
 });
 
 
-  $('.button-collapse').sideNav({
-      menuWidth: 300, // Default is 300
-      edge: 'right', // Choose the horizontal origin
-      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-      draggable: true // Choose whether you can drag to open on touch screens
-    }
-  );
+$('.button-collapse').sideNav({
+  menuWidth: 300, // Default is 300
+  edge: 'left', // Choose the horizontal origin
+  closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+  draggable: true // Choose whether you can drag to open on touch screens
+});
